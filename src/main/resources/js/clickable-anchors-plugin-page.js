9be6d35af19b0f80a21fa8c97afa8dbdb2c73099
Carsten Klein <trancesilken@gmail.com>
2018-10-05 19:23:19 +0200

    render heading as anchor instead prepare 1.0.1-ALPHA2 release

--- src/main/resources/js/clickable-anchors-plugin-page.js
+++ src/main/resources/js/clickable-anchors-plugin-page.js
@@ -3,23 +3,22 @@ AJS.$(document).ready(function () {
   var anchors = AJS.$("#content span.confluence-anchor-link");
   anchors.each(function (index, elt) {
 
+    var span = $(elt);
     var anchor = $(document.createElement("a"));
     anchor.attr("href", "#" + elt.id);
     anchor.attr("alt", elt.id);
     anchor.text("#");
     anchor.addClass("clickable-anchors-plugin-link");
-
-    AJS.$(elt).append(anchor);
+    anchor.appendTo(span);
   });
 
-  var headings = AJS.$("#content :header");
+  var headings = AJS.$("#content :header[id]");
   headings.each(function (index, elt) {
 
+    var heading = $(elt);
     var anchor = $(document.createElement("a"));
     anchor.attr("href", "#" + elt.id);
-    anchor.text("#");
-    anchor.addClass("clickable-anchors-plugin-link");
-
-    AJS.$(elt).append("&nbsp;").append(anchor);
+    heading.contents().appendTo(anchor);
+    anchor.appendTo(heading);
   });
 });
